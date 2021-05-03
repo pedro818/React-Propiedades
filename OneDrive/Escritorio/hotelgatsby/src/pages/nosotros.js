b76d75c4aaaee7e7dc760e6f9ca861dc5797732f
Pedrocode <pedro_cuello101@hotmail.com>
2021-05-03 16:12:36 -0300

    first commit

---
+++ OneDrive/Escritorio/hotelgatsby/src/pages/nosotros.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from '../components/layout';
+import ContenidoNostros from '../components/contenidoNosotros';
+
+const IndexPage = () => (
+  <Layout>
+    
+    <ContenidoNostros />
+  
+  </Layout>
+
+)
+
+export default IndexPage;
